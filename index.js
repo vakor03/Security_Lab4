d5b2ba40f0b958f7dbf197cc2db005879846fa3f
valerii.korniienko <vakor.dev@gmail.com>
2023-10-13 10:43:01 +0300

    All working correctly

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 const express = require('express');
 const axios = require('axios');
-const session = require('express-session');
+// const session = require('express-session');
 const bodyParser = require('body-parser');
+const path = require('path');
 const port = 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(
-    session({
-        secret: 'your-secret-key', // Change this to your secret key
-        resave: false,
-        saveUninitialized: true,
-    })
-);
+// app.use(
+//     session({
+//         secret: 'your-secret-key', // Change this to your secret key
+//         resave: false,
+//         saveUninitialized: true,
+//     })
+// );
 
 const auth0Domain = 'dev-x9vcburz.us.auth0.com';
 const auth0ClientId = 'Y5sCgikFYCrw8zybI08PXcwSZx1QHDJ8';
 const auth0ClientSecret = 'A4xnZDzfwueLvnw8xMCfBpoOx2HwVj_1ItxG_EawrQcFm6N-H2DXxXTPerFu8ylH';
 
 app.get('/', (req, res) => {
-    if (req.session.username) {
+    let token = req.header('Authorization');
+
+    if (token) {
+        token = token.replace("Bearer ", "");
+
+        axios
+            .get(`https://${auth0Domain}/userinfo`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((userInfo) => {
+                res.json({
+                    username: userInfo.data.name,
+                    logout: 'http://localhost:3000/logout',
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+                res.status(401).send('Authentication failed');
+            });
+
+    } else {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    }
+    if (req.session && req.session.username) {
         return res.json({
             username: req.session.username,
             logout: 'http://localhost:3000/logout',
         });
     }
-
-    res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/api/login', (req, res) => {
@@ -46,19 +70,6 @@ app.post('/api/login', (req, res) => {
         })
         .then((auth0Response) => {
             accessToken = auth0Response.data.access_token;
-
-            // Fetch user information from Auth0
-            return axios.get(`https://${auth0Domain}/userinfo`, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            });
-        })
-        .then((userInfo) => {
-            console.log(userInfo.data)
-            console.log(accessToken)
-            console.log(req.session)
-            req.session.username = userInfo.data.name;
             res.json({token: accessToken});
         })
         .catch((error) => {
@@ -69,7 +80,6 @@ app.post('/api/login', (req, res) => {
 
 
 app.get('/logout', (req, res) => {
-    req.session.username = undefined;
     res.redirect('/');
 });
 
