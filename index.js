60b8d36d9453a1712402fb72208e26c8d00dea24
valerii.korniienko <vakor.dev@gmail.com>
2023-10-13 09:45:50 +0300

    Updated index.js for Password grant_type

--- index.js
+++ index.js
@@ -1,137 +1,78 @@
-const uuid = require('uuid');
 const express = require('express');
-const onFinished = require('on-finished');
+const axios = require('axios');
+const session = require('express-session');
 const bodyParser = require('body-parser');
-const path = require('path');
 const port = 3000;
-const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+    session({
+        secret: 'your-secret-key', // Change this to your secret key
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
-const SESSION_KEY = 'Authorization';
+const auth0Domain = 'dev-x9vcburz.us.auth0.com';
+const auth0ClientId = 'Y5sCgikFYCrw8zybI08PXcwSZx1QHDJ8';
+const auth0ClientSecret = 'A4xnZDzfwueLvnw8xMCfBpoOx2HwVj_1ItxG_EawrQcFm6N-H2DXxXTPerFu8ylH';
 
-class Session {
-    #sessions = {}
-
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
-
-            console.log(this.#sessions);
-        } catch(e) {
-            this.#sessions = {};
-        }
-    }
-
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
-
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
-
-    get(key) {
-        return this.#sessions[key];
-    }
-
-    init(res) {
-        const sessionId = uuid.v4();
-        this.set(sessionId);
-
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
-}
-
-const sessions = new Session();
-
-app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
-
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
-    req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
-    });
-
-    next();
-});
-
-app.get('/', (req, res) =>  {
+app.get('/', (req, res) => {
     if (req.session.username) {
         return res.json({
             username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
+            logout: 'http://localhost:3000/logout',
+        });
     }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/api/login', (req, res) => {
+    const {login, password} = req.body;
+    let accessToken;
+
+    // Authenticate the user with Auth0 using the Password Grant Type
+    axios
+        .post(`https://${auth0Domain}/oauth/token`, {
+            grant_type: 'password',
+            username: login,
+            password: password,
+            scope: 'openid profile email',
+            client_id: auth0ClientId,
+            client_secret: auth0ClientSecret,
+        })
+        .then((auth0Response) => {
+            accessToken = auth0Response.data.access_token;
+
+            // Fetch user information from Auth0
+            return axios.get(`https://${auth0Domain}/userinfo`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+        })
+        .then((userInfo) => {
+            console.log(userInfo.data)
+            console.log(accessToken)
+            console.log(req.session)
+            req.session.username = userInfo.data.name;
+            res.json({token: accessToken});
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(401).send('Authentication failed');
+        });
+});
+
 
 app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
+    req.session.username = undefined;
     res.redirect('/');
 });
 
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
-    }
-]
-
-app.post('/api/login', (req, res) => {
-    const { login, password } = req.body;
-
-    const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
-        }
-        return false
-    });
-
-    if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({ token: req.sessionId });
-    }
-
-    res.status(401).send();
-});
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
